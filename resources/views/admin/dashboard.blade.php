437ab52d101b28f0add6b14633591233837f0a03
karuna531 <karunapandey125@gmail.com>
2023-04-19 11:31:06 +0545

    karuna changed

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,28 +1,36 @@
 @extends('admin.template')
 @section('content')
+<link rel="stylesheet" href="{{asset('site/css/admin.css')}}">
+
 <div class="container">
     <div class="row justify-content-center">
         <!-- if boold information  not exisit then only show -->
-       
-            <div class="col-md-12">
-                 <div class="card">
+
+        <div class="col-md-12">
+            <div class="card">
                 <div class="card-header">{{ __('Welcome to admin Dashboard') }}</div>
 
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md-12">
                             <h5>Your Blood Profile</h5>
-                           
-                        
-                        </div>
-                       
-                   </div>
 
-                
+
+                        </div>
+
+                    </div>
+
+
                 </div>
             </div>
-            </div>
-        
+        </div>
+
     </div>
 </div>
 @stop
+<!-- 
+
+
+
+
+ -->
