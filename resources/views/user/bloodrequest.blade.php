8fcf85bc1802a1cb04d577ff284b596a24ffa511
Sushmakhatri4949 <88888465+Sushmakhatri4949@users.noreply.github.com>
2023-06-14 22:56:34 +0545

    css added

--- resources/views/user/bloodrequest.blade.php
+++ resources/views/user/bloodrequest.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('content')
+<link rel="stylesheet" href="{{asset('site/css/requestedblood.css')}}">
 <div class="container">
     <div class="row justify-content-center">
             <div class="col-md-8">
--- resources/views/user/managerequestedblood.blade.php
+++ resources/views/user/managerequestedblood.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 @section('content')
+<link rel="stylesheet" href="{{asset('site/css/manage_req_blood.css')}}">
+
 <div class="container">
     <div class="row justify-content-center">
             <div class="col-md-8">
